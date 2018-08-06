5a540de2511604e6972164c5d561865574d5cf3c
Ben Camilleri <camilleriben@gmail.com>
2018-08-06 22:07:30 +0200

    Exercise 6- More Strings

---
+++ exercise_6-MoreStrings.py
@@ -0,0 +1,5 @@
+thisString = "this"
+print("String formatting can also be done like {}".format(thisString))
+
+print('Strings can also be written with single quoutes so you can use "double quotes"')
+print("'Single quotes'")
