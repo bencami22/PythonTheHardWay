11e90b400b350ed6a58929dcd3e2aae81352a150
Ben Camilleri <camilleriben@gmail.com>
2018-08-06 21:39:01 +0200

    Exercise 4-Variables

---
+++ exercise_4-Variables.py
@@ -0,0 +1,10 @@
+favourite_colour = "blue"
+my_age = 25
+my_girlfriends_age = 22
+age_difference = my_age - my_girlfriends_age
+a_floating_point_number=3.2
+
+print("My favourite colour is ", favourite_colour)
+print("My age is " , my_age)
+print("Age difference", age_difference)
+print("Age difference divided by a_floating_point_number", age_difference/a_floating_point_number)
