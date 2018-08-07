9ed7517890bd8365c1d28dae0b6f79a1149d766b
Ben Camilleri <camilleriben@gmail.com>
2018-08-07 21:16:50 +0200

    Exercise 13- Arguments

---
+++ exercise_13-Arguments.py
@@ -0,0 +1,6 @@
+from sys import argv
+script, first, second = argv
+
+print("The script is called", script)
+print("The first argument passed is ", first)
+print("The second argument passed is ", second)
