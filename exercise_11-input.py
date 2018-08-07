b24106ce9dbd7abd18bb0298c348d01b890c9d0d
Ben Camilleri <camilleriben@gmail.com>
2018-08-07 19:53:19 +0200

    Exercise 11- Input

---
+++ exercise_11-input.py
@@ -0,0 +1,9 @@
+print("how old are you?")
+age=input()
+print(f"You are {age} years old.")
+
+print("input your weight")
+weight=int(input())
+print("input your height")
+height=int(input())
+print("height x weight=",weight*height)
