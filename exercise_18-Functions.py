54365d43287032f8333c65df1143766e26305900
Ben Camilleri <camilleriben@gmail.com>
2018-08-09 20:49:41 +0200

    Exercise 18- Functions

---
+++ exercise_18-Functions.py
@@ -0,0 +1,11 @@
+def printArgs(*args):
+    arg1, arg2 = args
+    print(arg1)
+    print(arg2)
+
+def printArgsAgain(arg1, arg2):
+    print(arg1)
+    print(arg2)
+
+printArgs("1", "2")
+printArgsAgain("3", "4")
