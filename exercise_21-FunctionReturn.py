d4e856b530dec949e040dd1186e913dd8c64e74d
Ben Camilleri <camilleriben@gmail.com>
2018-08-09 21:29:03 +0200

    Exercise 21- functions with return values

---
+++ exercise_21-FunctionReturn.py
@@ -0,0 +1,12 @@
+def add(num1, num2):
+    return num1+num2
+
+def subtract(num1, num2):
+    return num1-num2
+
+num1=10
+num2=4
+print("num1=10   num2=4")
+
+print("num1+num2=", add(num1, num2))
+print("num1-num2=", subtract(num1, num2))
