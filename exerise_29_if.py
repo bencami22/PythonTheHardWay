9a0ac40487238af970b0aa9bd3f876e3ca232fe4
Ben Camilleri <camilleriben@gmail.com>
2018-08-11 15:28:44 +0200

    Exercise 29- If statement

---
+++ exerise_29_if.py
@@ -0,0 +1,26 @@
+people=20
+cats=30
+dogs=15
+
+if people < cats:
+    print("too many cats!")
+
+if people > cats:
+    print("more people than cats")
+
+if people < dogs:
+    print("the world is drooled on")
+
+if people > dogs:
+    print("the world is dry")
+
+dogs+=5
+
+if people >= dogs:
+    print("more people or same as dogs")
+
+if people <= dogs:
+    print("less people or same as dogs")
+
+if people ==  dogs:
+    print("same amount of people and dogs")
