bdb437b75c2115804dc64561da17adf1731271ad
Ben Camilleri <camilleriben@gmail.com>
2018-08-13 20:50:27 +0200

    Lists and Loops

---
+++ exercise_32_loops_and_lists.py
@@ -0,0 +1,20 @@
+the_counts = [1, 2, 3, 4, 5]
+colours = ["blue", "green", "red"]
+change = [1, "pennies", 2, "euro"]
+
+for number in the_counts:
+    print(number)
+
+for colour in colours:
+    print(colour)
+
+for unit in change:
+    print(unit)
+
+elements=[]
+
+for i in range(0, 6):
+    elements.append(i)
+
+for i in elements:
+    print(i)
