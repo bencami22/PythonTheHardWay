6323f80018358bfca5d0c87f21caa340cb422c42
Ben Camilleri <camilleriben@gmail.com>
2018-08-18 13:39:56 +0200

    Exercise 37- Keywords

---
+++ exercise_37_keywords.py
@@ -0,0 +1,31 @@
+and - Logical and
+as - Part of with-as statement
+assert - Assert that something is True
+break - stop the loop right now
+class - define a class
+continue - dont process more of the current loop, do it again
+def - define a function
+del- delete from dictionary
+elif - else if condition
+else - else condition
+except - if an exception happens, do this
+exec - run a string as python
+finally - exceptions or not, do this no matter what
+for - loop over a collection of things
+from - import specific parts of a module
+global -  declare that you want a global variable
+if - if condition
+import - import a module into this one
+in - part of for loops. also a test of X in Y.
+is - like == to test equality
+lambda - create a short anonymouse function
+not - logical not
+or - logical or
+pass - this block is Empty
+print - print this string
+raise - Raise an exception, go to except
+return - exit the function with a return value
+try - try this blockm if exception, go to except
+while - while loop
+with - with an expression as a variable do
+yield - pause here and return to caller
